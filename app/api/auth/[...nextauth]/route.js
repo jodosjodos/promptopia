2cda648aa417a52520b785348427701229b017e2
jodosjodos <jeandedieu2030@gmail.com>
2023-06-19 04:57:18 +0200

    working well sign in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,34 +10,41 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email });
-    session.user.id = sessionUser.id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    console.log(profile);
-    try {
-      await connectToDB();
+  callbacks:{
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+  
+      if(sessionUser){
 
-      // check if user already exists
-      const userExists = await User.findOne({ email: profile.email });
-
-      // if not save him
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        session.user.id = sessionUser.id.toString();
       }
+      return session;
+    },
+    async signIn({ profile }) {
+      
+      try {
+        await connectToDB();
+  
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+  
+        // if not save him
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+  
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  },
 });
 
 export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -39,7 +39,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -68,7 +68,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
